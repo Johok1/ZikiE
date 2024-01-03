230db825827d04739e1a4724660a5b4018146863
Johok1 <Joshua.hooks@yahoo.com>
2024-01-03 12:18:59 -0600

    package project

--- wiki-main/target/classes/static/js/newjs/login/new_login.js
+++ wiki-main/target/classes/static/js/newjs/login/new_login.js
@@ -29,7 +29,7 @@ class Model {
                         cookie.setCookie("rememberPassword", this.view.inputPassword.value)
                     }
                     cookie.setCookie("token", response, 1)
-                    window.location.href = "new_index.html"
+                    window.location.href = "https://www.zinxswiki.com"
                 } else if (response != "" && response != undefined) {
                     this.view.error.classList.remove("hidden")
                     this.view.error.innerHTML = response
@@ -49,7 +49,7 @@ function handleCredentialResponse(response) {
         .then(response => {
             console.log(response)
             if (response != "false") {
-                setCookie("token", response)
+                cookie.setCookie("token", response)
                 window.location.href = "https://www.zinxswiki.com"
             } else {
 

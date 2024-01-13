7efccdc5913ee1a61c07fb81694c1ce72ccdeac4
Johok1 <Joshua.hooks@yahoo.com>
2024-01-13 11:40:17 -0600

    forgot to add new endpoints to controller class

--- wiki-main/src/main/resources/static/js/newjs/wiki/controller.js
+++ wiki-main/src/main/resources/static/js/newjs/wiki/controller.js
@@ -3,6 +3,32 @@ export default class Controller {
         this.fetch_url_wiki = "http://localhost/api/v1/wiki"
     }
 
+    getWikiName(id) {
+        return fetch(this.fetch_url_wiki + "/getWikiName/" + id, {
+            method: 'GET',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'plain/text'
+            }
+        }).catch(error => {
+            console.error(error);
+        });
+    }
+
+    getWikiImg(id) {
+        return fetch(this.fetch_url_wiki + "/getWikiImage/" + id, {
+            method: 'GET',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'plain/text'
+            }
+        }).catch(error => {
+            console.error(error);
+        });
+    }
+
     getNewWiki(token) {
         return fetch(this.fetch_url_wiki + "/newWiki/" + token, {
             method: 'POST',

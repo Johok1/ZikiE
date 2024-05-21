004124af56aecd208373e49c65a4153d661dd5b6
Johok1 <Joshua.hooks@yahoo.com>
2024-05-21 17:14:50 -0500

    product foundation phase

--- wiki-main/src/main/java/zinxs/wiki/webdirectories/home/HomeDirectoryController.java
+++ wiki-main/src/main/java/zinxs/wiki/webdirectories/home/HomeDirectoryController.java
@@ -1,21 +1,37 @@
 package zinxs.wiki.webdirectories.home;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import zinxs.wiki.accountsapi.utilities.AuthTokenUtils;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping(path = "")
 public class HomeDirectoryController {
+
+    @Autowired
+    private AuthTokenUtils authTokenUtils;
+
     @CrossOrigin
     @GetMapping
-    public ModelAndView getLoginPage(){
+    public ModelAndView getLoginPage(@CookieValue(value = "token", defaultValue = "none") String token){
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("homepage-logged-out.html");
+        if(token.equals("none")){
+            modelAndView.setViewName("homepage-logged-out.html");
+        }else{
+            try {
+                if (authTokenUtils.isExpired(token)) {
+                    modelAndView.setViewName("homepage-logged-out.html");
+                } else {
+                    modelAndView.setViewName("homepage-logged-in.html");
+                }
+            }catch (Exception e){
+                modelAndView.setViewName("homepage-logged-out.html");
+            }
+        }
+
         return modelAndView;
     }
 }

ad1771ac01f041ec60a1ce20ac1e0cf58aa08b43
Johok1 <Joshua.hooks@yahoo.com>
2024-05-23 13:52:27 -0500

    ee

---
+++ wiki-main/target/classes/static/js/webmodules/register/modules/backend/components/controller.js
@@ -0,0 +1,27 @@
+export default class Controller {
+    constructor() {
+        this.fetch_url_validation ="https://www.zinxswiki.com/api/v1/validation"
+    }
+
+    postRegisterRequest(username, email, password) {
+        const registrationRequest = {
+            "username": username,
+            "email": email,
+            "password": password
+        };
+        return fetch(this.fetch_url_validation + "/static/login", {
+            method: 'POST',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(registrationRequest)
+        }).catch(error => {
+            console.error(error);
+        });
+    }
+
+  
+   
+
+}

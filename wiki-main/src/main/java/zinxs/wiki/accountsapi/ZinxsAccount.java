d30bf3e5b7beeb46ec1692f8fb550d17ddf25dc1
Johok1 <Joshua.hooks@yahoo.com>
2024-05-25 16:48:46 -0500

    ee

--- wiki-main/src/main/java/zinxs/wiki/accountsapi/ZinxsAccount.java
+++ wiki-main/src/main/java/zinxs/wiki/accountsapi/ZinxsAccount.java
@@ -4,12 +4,24 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
 
 @Entity
 public interface ZinxsAccount  {
+
+    @SequenceGenerator(
+            name = "zinxs_account_sequence",
+            sequenceName = "zinxs_account_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "zinxs_account_sequence"
+    )
+
 
     public String getUsername();
 

be2932a5e50a03a4c497e39144ec40f573be9289
Johok1 <Joshua.hooks@yahoo.com>
2024-05-31 18:10:39 -0500

    eee

--- wiki-main/target/classes/static/js/webmodules/pagewizard/modules/backend/components/controller.js
+++ wiki-main/target/classes/static/js/webmodules/pagewizard/modules/backend/components/controller.js
@@ -1,7 +1,7 @@
 export default class Controller {
     constructor() {
-        this.fetch_url_page = "https://www.zinxswiki.com/api/v1/page"
-        this.fetch_url_image = "https://www.zinxswiki.com/api/v1/image"
+        this.fetch_url_page = "https://www.zinxswiki.com/page"
+        this.fetch_url_image = "https://www.zinxswiki.com/image"
     }
 
     createAccountPage(token, name) {

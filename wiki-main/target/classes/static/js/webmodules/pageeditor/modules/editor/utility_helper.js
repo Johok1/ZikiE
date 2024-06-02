bd442a109128df28584d7ab12fb71c0b61eecbe9
Johok1 <Joshua.hooks@yahoo.com>
2024-06-01 19:11:13 -0500

    eee

--- wiki-main/target/classes/static/js/webmodules/pageeditor/modules/editor/utility_helper.js
+++ wiki-main/target/classes/static/js/webmodules/pageeditor/modules/editor/utility_helper.js
@@ -1,6 +1,6 @@
-import UtilityHandlerModule from './utility_modules/implements/utility_handler_module.js'
+import UtilityLayerHandlerModule from './utility_modules/implements/utility_handler_module.js'
 import UtilitySelectionModule from './utility_modules/implements/utility_selection_module.js'
-import UtilityTranslationModule from './utility_modules/implements/utility_translation_module.js'
+import UtilityLayerTranslationModule from './utility_modules/implements/utility_translation_module.js'
 
 import UtilityFactory from '../utilities/utility_factory.js'
 import LayerManagerModule from './utility_modules/implements/layer_manager_module.js'

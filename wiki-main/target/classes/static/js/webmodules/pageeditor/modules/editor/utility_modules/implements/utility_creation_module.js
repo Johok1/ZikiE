5b5fe537b79d8e9f249b641d0e4cc5dfbc1f0cb9
Johok1 <Joshua.hooks@yahoo.com>
2024-06-13 14:30:35 -0500

    UCM Sprint - Collision-Sensitive Utility Deployment
    in the ucm the utility is placed by a click event attached to the utility that removes transformations and places it in the correct spot, missing collision logic and a check to make sure its over the page.

--- wiki-main/target/classes/static/js/webmodules/pageeditor/modules/editor/utility_modules/implements/utility_creation_module.js
+++ wiki-main/target/classes/static/js/webmodules/pageeditor/modules/editor/utility_modules/implements/utility_creation_module.js
@@ -3,7 +3,7 @@ export default class UtilityCreationModule{
 
 		this.page = document.getElementById("page")
         this.utilityHelper = utilityHelper
-
+        this.backDrop = document.getElementById("layoutSidenav_content")
 	}
 
   
@@ -16,18 +16,25 @@ export default class UtilityCreationModule{
         register(select, layerManager.getCurrentSelectedLayer())
     }
 
-	createTextUtility = () => {
-        let layerManager = this.utilityHelper.layerManagerModule
-        let textUtility = this.utilityHelper.utilityFactory.constructTextUtility(layerManager.getCurrentSelectedLayer())
-        this.setUtilityPlacementMode(textUtility)
-
+    createTextUtility = () => {
+        if (!this.page.classList.contains("placing")) { 
+            this.page.classList.add("placing")
+            let layerManager = this.utilityHelper.layerManagerModule
+            let textUtility = this.utilityHelper.utilityFactory.constructTextUtility(layerManager.getCurrentSelectedLayer())
+            this.setUtilityPlacementMode(textUtility)
+         
+        }
         //this.registerAllHandlersSelect()
 	}
 
-	createImageUtility = () => {
-        let layerManager = this.utilityHelper.layerManagerModule
-        let imageUtility = this.utilityHelper.utilityFactory.constructImageUtility(layerManager.getCurrentSelectedLayer())
-        this.setUtilityPlacementMode(imageUtility)
+    createImageUtility = () => {
+        if (!this.page.classList.contains("placing")) {
+            this.page.classList.add("placing")
+            let layerManager = this.utilityHelper.layerManagerModule
+            let imageUtility = this.utilityHelper.utilityFactory.constructImageUtility(layerManager.getCurrentSelectedLayer())
+            this.setUtilityPlacementMode(imageUtility)
+            
+        }
 
         // this.registerAllHandlersSelect()
     }
@@ -38,13 +45,41 @@ export default class UtilityCreationModule{
 
         document.getElementById("layoutSidenav_content").appendChild(utility.element)
         utility.element.style.zIndex = "9999"
+        this.utility = utility
+        this.backDrop.onmousemove = this.stickUtilityToMouse.bind(this)
         
-        document.getElementById("layoutSidenav_content").addEventListener("mousemove", function (event) {
-            utility.element.style.transform = 'translateY(' + (event.clientY - 80) + 'px)'
-            utility.element.style.transform += 'translateX(' + (event.clientX - 50) + 'px)';
-         
-        })
+        utility.element.addEventListener("click", function (event) {
+            this.placeUtility(utility, event)
+        }.bind(this))
+
+        
        
+    }
+
+    stickUtilityToMouse = (event) => {
+        this.utility.element.style.transform = 'translateY(' + (event.clientY - 80) + 'px)'
+        this.utility.element.style.transform += 'translateX(' + (event.clientX - 50) + 'px)';
+
+    }
+
+    placeUtility = (utility, event) => {
+        if (this.page.classList.contains("placing")) {
+            this.backDrop.onmousemove = null
+            this.backDrop.removeChild(utility.element)
+            utility.element.style.position = "absolute"
+            utility.element.style.opacity = "100%"
+            this.page.appendChild(utility.element)
+            
+            
+            //utility.element.style.transform = 'translateY(' + (event.clientY-230) + 'px)'
+            //utility.element.style.transform += 'translateX(' + (event.clientX - 110) + 'px)';
+            utility.element.style.transform = ""
+            utility.element.style.left = `${event.clientX-110 }px`
+            utility.element.style.top = `${event.clientY-230 }px`
+            utility.enableDrag()
+            this.registerAllHandlersSelect()
+            this.page.classList.remove("placing")
+        }
     }
 
     //set utility to placement mode and add eventer for placement 

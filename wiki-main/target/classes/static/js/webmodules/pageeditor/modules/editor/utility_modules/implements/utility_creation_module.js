3d101bafacad4078f03d4a6839f1fc2bfe252e4b
Johok1 <Joshua.hooks@yahoo.com>
2024-06-13 11:17:38 -0500

    UCM Sprint - Placement Mode
    When you click the button to create a utility, you have an opaque version of the utility stuck to your mouse.

--- wiki-main/target/classes/static/js/webmodules/pageeditor/modules/editor/utility_modules/implements/utility_creation_module.js
+++ wiki-main/target/classes/static/js/webmodules/pageeditor/modules/editor/utility_modules/implements/utility_creation_module.js
@@ -19,17 +19,27 @@ export default class UtilityCreationModule{
 	createTextUtility = () => {
         let layerManager = this.utilityHelper.layerManagerModule
         let textUtility = this.utilityHelper.utilityFactory.constructTextUtility(layerManager.getCurrentSelectedLayer())
-
+        this.setUtilityPlacementMode(textUtility)
 
         //this.registerAllHandlersSelect()
 	}
 
 	createImageUtility = () => {
         let layerManager = this.utilityHelper.layerManagerModule
-       let imageUtility =  this.utilityHelper.utilityFactory.constructImageUtility(layerManager.getCurrentSelectedLayer())
-
+        let imageUtility = this.utilityHelper.utilityFactory.constructImageUtility(layerManager.getCurrentSelectedLayer())
+        this.setUtilityPlacementMode(imageUtility)
 
         // this.registerAllHandlersSelect()
+    }
+
+    setUtilityPlacementMode = (utility) => {
+        utility.element.style.position = "absolute"
+        utility.element.style.opacity = "50%"
+
+        document.addEventListener("mousemove", function ({ movementX, movementY }) {
+            utility.element.style.left = utility.element.style.left + movementX
+            utility.element.style.top = utility.element.style.top + movementY 
+        })
     }
 
     //set utility to placement mode and add eventer for placement 
